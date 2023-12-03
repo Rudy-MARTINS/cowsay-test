b5595224d5b7bb7858d33852e2f4e9dfff0322a6
RUDY MARTINS <martinsosr@gmail.com>
2023-12-03 17:57:15 +0100

    cowsay-2

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const cowsay = require('cowsay');
 
 console.log(cowsay.say({
   text: `Nom: ${information.nom}\nCampus: ${information.campus}`,
-  e: "oO", // Optionnel, c'est pour la tête de la vache
+  e: "x_x",
+  T : "$", 
 }));
